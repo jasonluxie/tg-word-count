540d7161eca8558b5d5b7d4befb0f592735b9fda
Jason Xie <jasonluxie@gmail.com>
2025-02-08 17:33:33 -0600

    added logic for word count from default data.json file that telegram provides

---
+++ index.ts
@@ -0,0 +1,56 @@
+import * as fs from 'fs';
+
+interface TextEntity {
+  type: string;
+  text: string;
+}
+
+interface Message {
+  id: number;
+  type: string;
+  date: string;
+  date_unixtime: string;
+  from: string;
+  from_id: string;
+  text: string | string[];
+  text_entities: TextEntity[];
+  edited?: string;
+  edited_unixtime?: string;
+  reactions?: any[];
+  photo?: string;
+  width?: number;
+  height?: number;
+}
+
+interface Chat {
+  name: string;
+  type: string;
+  id: number;
+  messages: Message[];
+}
+
+const data: Chat[] = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const wordCounts: { [key: string]: { name: string, count: number } } = {};
+const countWords = (text: string): number => {
+  return text.split(/\s+/).filter(word => word.length > 0).length;
+};
+
+data.forEach((chat: Chat) => {
+  chat.messages.forEach((message: Message) => {
+    const userId = message.from_id;
+    const userName = message.from;
+    const text = message.text;
+    const messageText = Array.isArray(text) ? text.join(' ') : text;
+    const wordCount = countWords(messageText);
+
+    if (wordCounts[userId]) {
+      wordCounts[userId].count += wordCount;
+    } else {
+      wordCounts[userId] = { name: userName, count: wordCount };
+    }
+  });
+});
+
+for (const userId in wordCounts) {
+  console.log(`${wordCounts[userId].name}: ${wordCounts[userId].count} words`);
+}
